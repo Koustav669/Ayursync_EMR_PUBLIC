c4a1a275d09e783ca31c9323f994cee20dc7bd22
nilavobasu-20 <nilavobasu@gmail.com>
2026-05-25 16:59:37 +0530

    Create App.tsx root component

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Desktop from './components/Desktop';
+import './App.css';
+
+function App(): React.ReactElement {
+  return (
+    <div className="app">
+      <Desktop />
+    </div>
+  );
+}
+
+export default App;
